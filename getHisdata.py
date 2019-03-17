14c0a624c6766a408c9e913a60dfedb26a36aa36
过宇 <guoyu@thtf.com.cn>
2019-03-17 16:46:52 +0800

    采用两种方法计算准确率：预测指数的准确率；预测涨跌的准确率

--- getHisdata.py
+++ getHisdata.py
@@ -6,34 +6,22 @@ import pandas as pd
 from rqalpha.api import *
 import datetime
 
-# 在这个方法中编写任何的初始化逻辑。context对象将会在你的算法策略的任何方法之间做传递。
-def init(context):
-    logger.info("init")
-    context.s1 = "000001.XSHE"
-    update_universe(context.s1)
-    # 是否已发送了order
-    context.fired = False
-    context.cnt = 1
+#只能在米筐平台上运行，这里作为备份
 
+#get_price('中证500', start_date='2016-02-03', end_date='2016-02-13', frequency='1m', fields=None)
+df = pd.DataFrame(get_price('中证500', start_date='2007-01-15', end_date=datetime.date.today(), frequency='1d', fields=['open','close','high','low','volume','total_turnover']))
+dfPEPB = pd.DataFrame(index_indicator('中证500', start_date='2000-02-03', end_date=datetime.date.today()))
+#将dfPEPB中交易日期列作为索引
+dfPEPB.set_index(["trade_date"], inplace=True)
+#将dfPEPB中PE和PB两行加到df后面
+df['pb'] = dfPEPB['pb']
+df['pe_ttm'] = dfPEPB['pe_ttm']
+dfTomorrow = pd.DataFrame(df['close'])
+#删除df第一行
+df = df.drop(index=[df.index[len(df)-1]])
+df.to_csv('ZZ500.csv')
+#删除dfTomorrow最后一行
+#预测算法是用df的数据预测dfTomorrow的中证500指数
+dfTomorrow =dfTomorrow.drop(index=[dfTomorrow.index[0]])
+dfTomorrow.to_csv('ZZ500Tomorrow.csv')
 
-def before_trading(context):
-    logger.info("Before Trading", context.cnt)
-    context.cnt += 1
-
-
-# 你选择的证券的数据更新将会触发此段逻辑，例如日或分钟历史数据切片或者是实时数据切片更新
-def handle_bar(context, bar_dict):
-    context.cnt += 1
-    logger.info("handle_bar", context.cnt)
-    # 开始编写你的主要的算法逻辑
-
-    # bar_dict[order_book_id] 可以拿到某个证券的bar信息
-    # context.portfolio 可以拿到现在的投资组合状态信息
-
-    # 使用order_shares(id_or_ins, amount)方法进行落单
-
-    # TODO: 开始编写你的算法吧！
-    if not context.fired:
-        # order_percent并且传入1代表买入该股票并且使其占有投资组合的100%
-        order_percent(context.s1, 1)
-        context.fired = True
