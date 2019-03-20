bc6f480e667f1ad030834fd770485d2965eadd8b
过宇 <guoyu@thtf.com.cn>
2019-03-20 17:33:05 +0800

    SVR

--- svr.py
+++ svr.py
@@ -12,7 +12,7 @@ import time
 from sklearn import datasets
 from sklearn.svm import SVR
 from sklearn.model_selection import train_test_split
-svr = SVR(kernel = 'rbf',C = 1e3,gamma = 0.01)
+svr = SVR(kernel = 'rbf',C = 1e3,gamma = 0.03)
 #svr = SVR(C=1.0, cache_size=200, coef0=0.0, degree=3, epsilon=0.2, gamma=0.1,kernel='rbf', max_iter=-1, shrinking=True, tol=0.001, verbose=False)
 
 def createModel():
@@ -154,7 +154,7 @@ def accuracy2(predict, true):
     return sizeofright/sizeofall
 
 def example():
-    n_samples, n_features = 3000, 25
+    n_samples, n_features = 88, 30
     np.random.seed(0)
     Y = np.random.randn(n_samples)
     X = np.random.randn(n_samples, n_features)
